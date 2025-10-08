fa7fcece5171028b12669b2fec7478837e66cca0
trebuszeq <trebuszeq@gmail.com>
2025-10-08 09:33:25 +0200

    180 consecutive numbers

--- a.sql
+++ a.sql
@@ -1,1 +1,7 @@
 --- https://leetcode.com/problems/consecutive-numbers/
+SELECT DISTINCT b.num as ConsecutiveNums
+FROM Logs a
+JOIN Logs b ON a.num = b.num
+    AND b.id = a.id - 1
+JOIN Logs c ON a.num = c.num
+    AND c.id = a.id + 1
