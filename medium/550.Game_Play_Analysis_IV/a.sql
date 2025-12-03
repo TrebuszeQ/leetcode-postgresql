cf3ecb22951727d8c7ec10d1a695af58874f9d34
TrebuszeQ <trebuszeq@gmail.com>
2025-12-03 11:37:08 +0100

    game_play_analysis_iv

--- medium/550.Game_Play_Analysis_IV/a.sql
+++ medium/550.Game_Play_Analysis_IV/a.sql
@@ -1,18 +1,28 @@
---https://leetcode.com/problems/game-play-analysis-iv/
-    
+-- https://leetcode.com/problems/game-play-analysis-iv/
+WITH CTE AS (
+    SELECT player_id,
+        event_date
+    FROM (
+        SELECT *,
+            ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date ASC, player_id ASC) AS row_number
+        FROM Activity
+    ) AS ranked_entries
+    WHERE row_number = 1
+        OR row_number = 2
+)
 
 SELECT ROUND(
     (
         (
             SELECT COUNT(DISTINCT a.player_id)
-            FROM Activity a
-            JOIN Activity b
+            FROM CTE a
+            JOIN CTE b
                 ON a.player_id = b.player_id
-            WHERE a.event_date = b.event_date + 1
+            WHERE b.event_date = a.event_date + 1
         )::numeric /
         (
-            SELECT COUNT(DISTINCT player_id) 
-            FROM Activity
+            SELECT COUNT(DISTINCT player_id)
+            FROM CTE
         )::numeric
     ),
     2
